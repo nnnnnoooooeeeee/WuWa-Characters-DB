cf2172733ba87bac89c57203999dd6e1fba526ac
nnnnnoooooeeeee <yuukikuroha91@gmail.com>
2025-12-17 11:03:34 +0700

    Add files via upload

---
+++ js/yt.js
@@ -0,0 +1,8 @@
+function showVideo() {
+    var container = document.getElementById('videoContainer');
+    container.style.display = 'flex';
+}
+function closeVideo() {
+    var container = document.getElementById('videoContainer');
+    container.style.display = 'none';
+}
